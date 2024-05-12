505e60719fa7bd811a269292c9c376a302c32c7e
Jorge Miguel V. da Silva <jorgemvs578@gmail.com>
2024-05-12 15:44:35 -0300

    nav sendo criada e css dos cards em andamento

--- ProjetoReact/filme-avaliacao/src/components/maisBuscados/maisBuscados.js
+++ ProjetoReact/filme-avaliacao/src/components/maisBuscados/maisBuscados.js
@@ -1,6 +1,38 @@
+import { useState, useEffect } from "react";
+import "./stylesCard.css";
+
 const MaisBuscados = () => {
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+        const buscarFilmes = async () => {
+            try {
+                const resposta = await fetch("http://localhost:8080/filmes");
+                const dados = await resposta.json();
+                console.log(dados);
+                setFilmes(dados);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        buscarFilmes();
+    }, []);
+
     return (
-        <h1>MaisBuscados</h1>
+        <div className="card-container">
+            {filmes.map((filmes, index) => (
+                <div className="card" key={index}>
+                    <h1>{filmes.titulo}</h1>
+                    <h1>{filmes.genero}</h1>
+                    <h1>{filmes.ano}</h1>
+                    <h1>{filmes.categoria}</h1>
+                    <h1>{filmes.duracao}</h1>
+                    <h1>{filmes.descricao}</h1>
+                    <img src={filmes.imagem} alt={filmes.titulo} />
+
+                </div>
+            ))}
+        </div>
     );
 }
 
--- ProjetoReact/filme-avaliacao/src/components/pesquisa/pesquisa.js
+++ ProjetoReact/filme-avaliacao/src/components/pesquisa/pesquisa.js
@@ -1,6 +1,21 @@
+import { Link } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi"
+
 const Pesquisa = () => {
     return (
-        <h1>Pesquisa</h1>
+        <nav id="navbar">
+            <h2>
+                <Link to="/">
+                    <BiCameraMovie />  Movie Opinion
+                </Link>
+            </h2>
+            <form>
+                <input type="text" placeholder="Busque um filme" />
+                <button type="submit">
+                    <BiSearchAlt2 />
+                </button>
+            </form>
+        </nav>
     );
 }
 
